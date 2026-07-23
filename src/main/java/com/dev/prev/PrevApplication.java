18475b43d285804fc9d49f21f6fd3966dcbc2c01
Divyesh  Shivakumar <divyeshshivakumar@Divyeshs-MacBook-Air.local>
2026-07-23 21:42:20 +0530

    initial commit

---
+++ src/main/java/com/dev/prev/PrevApplication.java
@@ -0,0 +1,13 @@
+package com.dev.prev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/prev/PrevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.prev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
